78224dfdd5927a8eac76481ca25f122a2eac17cc
Michée Allidjinou <allidjinoumichee@outlook.fr>
2022-12-11 23:15:35 +0100

    feat: add mongodb connection
    Connect to local mongodb database.
    Import `Campground` model.

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/Campground');
+
+mongoose.set('strictQuery', false); // Suppress deprecation warning
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind('Connection error: '));
+db.once('open', () => {
+  console.log('Database connected.');
+})
 
 const app = express();
 
